4d782049a09189ba23841426e92358a668372a2b
Alexander Makeev <2695ma@gmail.com>
2017-10-07 12:43:47 +0300

    Initial commit

---
+++ src/main/java/com/example/rvs/RvsApplication.java
@@ -0,0 +1,12 @@
+package com.example.rvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvsApplication.class, args);
+	}
+}
